221cf5c346c5436037b5948f40811aa22b546045
Michael Ran <michaelran16@gmail.com>
2018-04-12 17:02:38 -0700

    example code

--- trade_aggregation.go
+++ trade_aggregation.go
@@ -9,12 +9,15 @@ import (
 func main() {
 	fmt.Println("Test trade aggregation")
 
-	trades, err := horizon.DefaultTestNetClient.LoadTradeAggregation()
+	trades, err := horizon.DefaultTestNetClient.LoadTradeAggregations(horizon.Asset{"credit_alphanum4", "CMA", "GD6OHWXPO7T46SEL2SNAFKGHLQZQ467U6MMDFKZ6BR3UXCINQ4BP3IZA"}, horizon.Asset{Type: "native"}, 300000, horizon.Limit(10), horizon.Order(horizon.OrderAsc), horizon.StartTime(1522318000000), horizon.EndTime(1522320000000))
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Result is ", trades.Embedded.Records)
+	for i, rr := range trades.Embedded.Records {
+		fmt.Println(i)
+		fmt.Println(rr)
+	}
 
 	fmt.Println("Account testing finished")
 }
